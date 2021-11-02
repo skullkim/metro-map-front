ba3ec8630b065fbe69f51c7beba76bf5af61b128
skullKim <kyk990328@gmail.com>
2021-11-02 15:38:11 +0900

    수정: 최근 검색 내역 모달창 디자인 바뀜으로 인한 디자인 코드 수정

--- src/components/modal/searchHistoryModal/SearchHistoryModal.js
+++ src/components/modal/searchHistoryModal/SearchHistoryModal.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-import { ModalBox, CommonModalBox, DataTitle, ModalTitle } from '../CommonModal';
+import { ModalBox, CommonModalBox, ModalTitle } from '../CommonModal';
 import ModalCloseButton from '../ModalCloseButton';
 
 const ModalTitleBox = styled.section`
@@ -15,36 +15,35 @@ const ModalTitleBox = styled.section`
   }
 `;
 
-const StartArriveTitle = styled(DataTitle)`
-  margin-top: 30px;
+const StartArriveTitle = styled.th`
   align-self: center;
   font-size: 20px;
+  font-weight: bold;
 `;
 
-const HistoryBox = styled.section`
-  height: 200px;
-  width: 200px;
+const HistoryTable = styled.table`
+  height: 300px;
+  width: 400px;
   position: relative;
-  top: 25px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  
-  ${DataTitle} {
-    margin-top: 15px;
-    margin-right: 10px;
-  }
+  top: 30px;
 `;
 
-const DataBox = styled.div`
-  width: 150px;
-  display: flex;
-  justify-content: center;
+
+const TableData = styled.td`
+  height: 10px;
+  text-align: center;
+  font-weight: bold;
+  font-size: 20px
 `;
+
+const TableRow = styled.tr`
+  height: 10px;
+`;
+
 
 const CloseButton = styled.section`
   position: relative;
-  top: 100px;
+  top: 50px;
 `;
 
 const SearchHistoryModal = () => {
@@ -53,38 +52,103 @@ const SearchHistoryModal = () => {
       <ModalBox>
         <ModalTitleBox>
           <ModalTitle>최근 검색 내역</ModalTitle>
-          <StartArriveTitle>출발역 / 도착역</StartArriveTitle>
         </ModalTitleBox>
-        <HistoryBox>
-          <DataBox>
-            <DataTitle>120--&gt;806</DataTitle>
-            <img src='img/bookmark.svg' alt='bookmark' />
-          </DataBox>
-          <DataBox>
-            <DataTitle>120--&gt;806</DataTitle>
-            <img src='img/unbookmark.svg' alt='bookmark' />
-          </DataBox>
-          <DataBox>
-            <DataTitle>120--&gt;806</DataTitle>
-            <img src='img/bookmark.svg' alt='bookmark' />
-          </DataBox>
-          <DataBox>
-            <DataTitle>120--&gt;806</DataTitle>
-            <img src='img/bookmark.svg' alt='bookmark' />
-          </DataBox>
-          <DataBox>
-            <DataTitle>120--&gt;806</DataTitle>
-            <img src='img/bookmark.svg' alt='bookmark' />
-          </DataBox>
-          <DataBox>
-            <DataTitle>120--&gt;806</DataTitle>
-            <img src='img/bookmark.svg' alt='bookmark' />
-          </DataBox>
-          <DataBox>
-            <DataTitle>120--&gt;806</DataTitle>
-            <img src='img/bookmark.svg' alt='bookmark' />
-          </DataBox>
-        </HistoryBox>
+        <HistoryTable bold='1'>
+          <thead>
+            <tr>
+              <StartArriveTitle>최소비용</StartArriveTitle>
+              <StartArriveTitle>출발역 / 도착역</StartArriveTitle>
+              <StartArriveTitle>경유지</StartArriveTitle>
+              <StartArriveTitle>즐겨찾기</StartArriveTitle>
+            </tr>
+          </thead>
+          <tbody>
+            <TableRow>
+              <TableData>
+                시간
+              </TableData>
+              <TableData>
+                120--&gt;806
+              </TableData>
+              <TableData>
+                102
+              </TableData>
+              <TableData>
+                <img src='img/bookmark.svg' alt='bookmark' />
+              </TableData>
+            </TableRow>
+            <TableRow>
+              <TableData>
+                시간
+              </TableData>
+              <TableData>
+                120--&gt;806
+              </TableData>
+              <TableData>
+                102
+              </TableData>
+              <TableData>
+                <img src='img/bookmark.svg' alt='bookmark' />
+              </TableData>
+            </TableRow>
+            <TableRow>
+              <TableData>
+                시간
+              </TableData>
+              <TableData>
+                120--&gt;806
+              </TableData>
+              <TableData>
+                102
+              </TableData>
+              <TableData>
+                <img src='img/bookmark.svg' alt='bookmark' />
+              </TableData>
+            </TableRow>
+            <TableRow>
+              <TableData>
+                시간
+              </TableData>
+              <TableData>
+                120--&gt;806
+              </TableData>
+              <TableData>
+                102
+              </TableData>
+              <TableData>
+                <img src='img/bookmark.svg' alt='bookmark' />
+              </TableData>
+            </TableRow>
+            <TableRow>
+              <TableData>
+                시간
+              </TableData>
+              <TableData>
+                120--&gt;806
+              </TableData>
+              <TableData>
+                102
+              </TableData>
+              <TableData>
+                <img src='img/bookmark.svg' alt='bookmark' />
+              </TableData>
+            </TableRow>
+            <TableRow>
+              <TableData>
+                시간
+              </TableData>
+              <TableData>
+                120--&gt;806
+              </TableData>
+              <TableData>
+                X
+              </TableData>
+              <TableData>
+                <img src='img/unbookmark.svg' alt='bookmark' />
+              </TableData>
+            </TableRow>
+          </tbody>
+        </HistoryTable>
         <CloseButton>
           <ModalCloseButton />
         </CloseButton>
