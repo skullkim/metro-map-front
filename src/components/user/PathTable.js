ef0a1d48d9836e14724c6c0637a18486f51abfc3
skullKim <kyk990328@gmail.com>
2021-11-11 18:47:34 +0900

    추가: 유실물센터 페이지 데모 완료

--- src/components/user/PathTable.js
+++ src/components/user/PathTable.js
@@ -1,43 +1,17 @@
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
 
 import { SearchStationOption } from '../../lib/subwayData';
-
-const PathListTitle = styled.th`
-  align-self: center;
-  font-size: 20px;
-  font-weight: bold;
-`;
-
-const PathListTable = styled.table`
-  height: 300px;
-  width: 400px;
-  position: relative;
-  top: 30px;
-`;
-
-
-const TableData = styled.td`
-  height: 10px;
-  text-align: center;
-  font-weight: bold;
-  font-size: 20px
-`;
-
-const TableRow = styled.tr`
-  height: 10px;
-`;
-
+import { ListTable, ListTitle, TableData, TableRow } from '../styles/Table';
 
 const PathTable = ({pathLists, handleClick}) => {
   return (
-    <PathListTable>
+    <ListTable>
       <thead>
         <tr>
-          <PathListTitle>최소비용</PathListTitle>
-          <PathListTitle>출발역 / 도착역</PathListTitle>
-          <PathListTitle>경유지</PathListTitle>
-          <PathListTitle>즐겨찾기</PathListTitle>
+          <ListTitle>최소비용</ListTitle>
+          <ListTitle>출발역 / 도착역</ListTitle>
+          <ListTitle>경유지</ListTitle>
+          <ListTitle>즐겨찾기</ListTitle>
         </tr>
       </thead>
       <tbody>
@@ -62,7 +36,7 @@ const PathTable = ({pathLists, handleClick}) => {
           );
         }) : null}
       </tbody>
-    </PathListTable>
+    </ListTable>
   );
 }
 
--- src/pages/LostAndFound.js
+++ src/pages/LostAndFound.js
@@ -1,7 +1,36 @@
+import styled from 'styled-components';
+
+import { Wrapper } from '../components/styles/Authorization';
+import PageTitle from '../components/styles/PageTitle';
+import { ListTable, ListTitle, TableData, TableRow } from '../components/styles/Table';
+
+const LostAndFoundBox = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
 const LostAndFound = () => {
   return (
-    <></>
+    <Wrapper>
+      <LostAndFoundBox>
+        <PageTitle>유실물센터 목록</PageTitle>
+        <ListTable>
+          <thead>
+            <ListTitle>역</ListTitle>
+            <ListTitle>전화번호</ListTitle>
+            <ListTitle>운영시간</ListTitle>
+          </thead>
+          <tbody>
+            <TableRow>
+              <TableData>105</TableData>
+              <TableData>12-345-6789</TableData>
+              <TableData>09:00~18:00</TableData>
+            </TableRow>
+          </tbody>
+        </ListTable>
+      </LostAndFoundBox>
+    </Wrapper>
   );
 }
 
