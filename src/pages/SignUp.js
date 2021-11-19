7d19fd14376f1e7d19e7b0af7c037ff8f34f75bd
skullKim <kyk990328@gmail.com>
2021-11-19 14:32:30 +0900

    리팩토링: 필요 없는 코드 삭제

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,24 +1,3 @@
-// import  {useFormik} from "formik";
-// import {useCallback, useState} from 'react';
-// import * as yup from 'yup';
-//
-// import {
-//   Form,
-//   Input,
-//   InputTitle,
-//   LinkMessage,
-//   PageTitle,
-//   SubmitBtn,
-//   Wrapper
-// } from "../components/styles/Authorization";
-// import { Warning, Success } from "../components/styles/ResultMessage";
-// import { Api } from "../lib/customAxios";
-// import { ClientPath, ServerPath } from "../lib/dataPath";
-// import { maxLen, regExp, warning } from "../lib/validateUserInfo";
-
-
-
-
 import ValidateUserAccount from '../components/user/ValidateUserAccount';
 import { authData } from '../lib/authenticateData';
 
